ee4b0f79719906dae45b8b272250056003399e0f
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-10-13 17:47:54 -0500

    casi terminado

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    <button @click="btnCancelar" >Cancelar</button>
-    <button
-      @mouseover="moveButton"
-      @click="moveButton"
-      :style="{ top: buttonTop + 'px', left: buttonLeft + 'px' }"
-    >
-      Mueve el Botón
-    </button>
+  <div class="centered-container">
+    <div class="row">
+      <button @click="btnCancelar">Cancelar</button>
+      <button
+        class="btnAceptar"
+        @mouseover="moveButton"
+        @click="moveButton"
+        :style="{ top: buttonTop + 'px', left: buttonLeft + 'px' }"
+        ref="moveableButton"
+      >
+        Mueve el Botón
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,29 +19,51 @@
 export default {
   data() {
     return {
-      buttonTop: 0,
-      buttonLeft: 0,
+      buttonTop: 257,
+      buttonLeft: 750,
+      isFirstMove: true,
     };
   },
   methods: {
     moveButton() {
-      // Calcula nuevas posiciones de manera aleatoria
-      const newTop = Math.floor(Math.random() * window.innerHeight);
-      const newLeft = Math.floor(Math.random() * window.innerWidth);
+      // Si es la primera vez, establece la posición inicial
+      if (this.isFirstMove) {
+        this.isFirstMove = false;
+        this.$refs.moveableButton.style.top = this.buttonTop + 'px';
+        this.$refs.moveableButton.style.left = this.buttonLeft + 'px';
+      } else {
+        // Calcula nuevas posiciones de manera aleatoria
+        const newTop = Math.floor(Math.random() * window.innerHeight);
+        const newLeft = Math.floor(Math.random() * window.innerWidth);
 
-      // Actualiza las posiciones en los datos del componente
-      this.buttonTop = newTop;
-      this.buttonLeft = newLeft;
+        // Actualiza las posiciones en los datos del componente
+        this.buttonTop = newTop;
+        this.buttonLeft = newLeft;
+        console.log(newLeft, newTop);
+      }
     },
-    btnCancelar(){
+    btnCancelar() {
       alert("Ayrton2");
-    }
+    },
   },
 };
 </script>
 
 <style>
+.btnAceptar {
+  position: absolute; /* Asegura que la posición absoluta funcione correctamente */
+}
+
+.centered-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Esto asegura que el contenedor ocupe el 100% de la altura de la ventana */
+}
+
 button {
-  position: absolute;
+  margin: 0 15px; /* Margen de 15px entre los botones */
+  padding: 10px 20px;
+  /* Agrega otros estilos según sea necesario */
 }
 </style>
