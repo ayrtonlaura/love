b0a1f1c9d712423e3da3941d419a07ab1effed1f
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-10-13 17:08:16 -0500

    githup page subido actualizado

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -15,7 +15,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  
+  history: createWebHashHistory(),
   routes
 })
 
