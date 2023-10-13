898cd38e7f1660f2aace58534199652addca5f2e
Ayrton Laura <ayrtonlauracaja@gmail.com>
2023-10-13 16:03:21 -0500

    primer commit love

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <button @click="btnCancelar" >Cancelar</button>
+    <button
+      @mouseover="moveButton"
+      @click="moveButton"
+      :style="{ top: buttonTop + 'px', left: buttonLeft + 'px' }"
+    >
+      Mueve el Botón
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      buttonTop: 0,
+      buttonLeft: 0,
+    };
+  },
+  methods: {
+    moveButton() {
+      // Calcula nuevas posiciones de manera aleatoria
+      const newTop = Math.floor(Math.random() * window.innerHeight);
+      const newLeft = Math.floor(Math.random() * window.innerWidth);
+
+      // Actualiza las posiciones en los datos del componente
+      this.buttonTop = newTop;
+      this.buttonLeft = newLeft;
+    },
+    btnCancelar(){
+      console.log("Ayrton");
+    }
+  },
+};
+</script>
+
+<style>
+button {
+  position: absolute;
+}
+</style>
